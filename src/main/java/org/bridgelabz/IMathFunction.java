82f3646e397cda96908b7527bc56f4f9cdd3c76c
datta1808 <bagaledatta18@gmail.com>
2021-05-31 19:51:26 +0530

    Pass Lambda Function to Show Result

--- src/main/java/org/bridgelabz/IMathFunction.java
+++ src/main/java/org/bridgelabz/IMathFunction.java
@@ -3,4 +3,8 @@ package org.bridgelabz;
 @FunctionalInterface
 interface IMathFunction{
     int calculate(int a,int b);
+    //Passing Lambda Expression as Function Parameter yo Print Result using Static Function
+    static void printResult(int a , int b ,String function, IMathFunction fobj){
+        System.out.println("Result of " + function + " is " + fobj.calculate(a, b));
+    }
 }
--- src/main/java/org/bridgelabz/MathOperation.java
+++ src/main/java/org/bridgelabz/MathOperation.java
@@ -4,18 +4,24 @@ public class MathOperation {
 
     public static void main(String[] args) {
         //use case 1
-        //way 1 ---IMathFunction add = ( int a, int b)->a+b;
+        //way 1 --- IMathFunction add = ( int a, int b) -> a + b;
         IMathFunction add = Integer::sum;
         //multiply
-        IMathFunction multiply = ( int a, int b) -> a*b;
+        IMathFunction multiply = (int a, int b) -> a * b;
         //division
-        IMathFunction division = ( int a, int b) -> a/b;
+        IMathFunction division = (int a, int b) -> a / b;
         //subtract
-        IMathFunction subtract = ( int a, int b) -> a-b;
+        IMathFunction subtract = (int a, int b) -> a - b;
 
         System.out.println("Addition " + add.calculate(6 ,3));
         System.out.println("Multiply " + multiply.calculate(6 ,3));
         System.out.println("Division " + division.calculate(6 ,3));
         System.out.println("Subtract " + subtract.calculate(6 ,3));
+
+        //Passing Lambda Expression as Function Parameter yo Print Result using Static Function
+        IMathFunction.printResult(6,3,"Addition", add);
+        IMathFunction.printResult(6,3,"Subtract", subtract);
+        IMathFunction.printResult(6,3,"Multiply", multiply);
+        IMathFunction.printResult(6,3,"Division", division);
     }
 }
